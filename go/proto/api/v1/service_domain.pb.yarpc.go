1cd936eb8e24d42f8d8cd38d6216e619c14c48d5
Tim Li <ltim@uber.com>
2024-07-23 15:31:33 -0700

    Update IDL SHA

--- go/proto/api/v1/service_domain.pb.yarpc.go
+++ go/proto/api/v1/service_domain.pb.yarpc.go
@@ -1,17 +1,17 @@
 // The MIT License (MIT)
-// 
+//
 // Copyright (c) 2021 Uber Technologies, Inc.
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -237,12 +237,18 @@ func NewFxDomainAPIYARPCProcedures() interface{} {
 				Server:      params.Server,
 				AnyResolver: params.AnyResolver,
 			}),
-			ReflectionMeta: reflection.ServerMeta{
-				ServiceName:     "uber.cadence.api.v1.DomainAPI",
-				FileDescriptors: yarpcFileDescriptorClosure2e37d15268893114,
-			},
+			ReflectionMeta: DomainAPIReflectionMeta,
 		}
 	}
+}
+
+// DomainAPIReflectionMeta is the reflection server metadata
+// required for using the gRPC reflection protocol with YARPC.
+//
+// See https://github.com/grpc/grpc/blob/master/doc/server-reflection.md.
+var DomainAPIReflectionMeta = reflection.ServerMeta{
+	ServiceName:     "uber.cadence.api.v1.DomainAPI",
+	FileDescriptors: yarpcFileDescriptorClosure2e37d15268893114,
 }
 
 type _DomainAPIYARPCCaller struct {
